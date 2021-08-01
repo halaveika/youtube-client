b43a27af993d45791beb13c08640a1fdc9a849dc
halaveika <halaveika1988@gmail.com>
2021-08-01 10:05:19 +0300

    feat: add app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderLoginComponent } from './components/header-login/header-login.component';
+import { HeaderFilterComponent } from './components/header-filter/header-filter.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { SearchItemComponent } from './components/search-item/search-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    HeaderLoginComponent,
+    HeaderFilterComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
   ],
   imports: [
     BrowserModule,
