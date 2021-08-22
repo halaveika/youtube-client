3f3a1e5620bb85e6c8acc9d55e488f78ad47e856
halaveika <halaveika1988@gmail.com>
2021-08-22 21:32:37 +0300

    fix: change youtube api token

--- src/app/shared/constans.ts
+++ src/app/shared/constans.ts
@@ -3,4 +3,4 @@ export const DESC = 'DESC';
 export const TOKEN = 'authtoken';
 export const URL_YOUTUBE_LIST = 'https://www.googleapis.com/youtube/v3/search';
 export const URL_YOUTUBE_VIDEO = 'https://www.googleapis.com/youtube/v3/videos';
-export const YOUTUBE_API_TOKEN = 'AIzaSyA86NkGAFZ8U9zkA8oEwvuz2oEIsZKge-0';
+export const YOUTUBE_API_TOKEN = 'AIzaSyC0QeoU37U9TIxF2-QWC80DUTy7PE1OI7I';
